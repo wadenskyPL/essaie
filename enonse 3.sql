a756b30c6054ca12470afa11ef5145199dadb912
wadenskyPL <112264554+wadenskyPL@users.noreply.github.com>
2023-08-07 16:36:02 -0400

    Add files via upload

---
+++ enonse 3.sql
@@ -0,0 +1,10 @@
+create database monde ;
+ 
+ create table peyi(  id_peyi int ,    non_peyi varchar (30), 
+    kapital varchar (30),  
+      lang_ofisyel  varchar (30),    lajan varchar (30),   
+  sipefisi DECIMAL(10,2),
+      Popilasyon DECIMAL (10,2)   
+       );
+
+       Describe peyi;
